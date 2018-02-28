5fa2f5deeb8d6a96dfa08c5344f715a54819d6df
brox27 <oystein.brox@gmail.com>
2018-02-28 13:58:31 +0100

    final cleanup

--- backend/server.py
+++ backend/server.py
@@ -9,30 +9,22 @@ import numpy as np
 from flask import *
 app = Flask(__name__)
 CORS(app)
-
-
-globalList = []
-
-
-
-
 print("PID: ", os.getpid())
 
 def Pushtodb(Snappedlist):
     for row in Snappedlist:
         NewDB.insertToDb(row[0],row[1],row[2],row[3])
 
-
 @app.route("/")
 def index():
     return "Index!"
+
 @app.route("/hello")
 def hello():
     return "Hello World!"
 
 @app.route('/events', methods=['POST'])
 def events():
-  #  positionlist = []
     idn = np.array(request.form.getlist('id'))
     lat = np.array(request.form.getlist('lat'))
     lng = np.array(request.form.getlist('lng'))
@@ -46,7 +38,6 @@ def events():
         temp = json.loads(fromDB)
         prevOldest = np.array([[temp['id'], temp['lat'], temp['lng'], temp['time']]])
         data = np.concatenate([prevOldest, data], axis=0)
-
     snappedlist = snap.snap_to_road(data, interpolate=False)
 
     #Delete overlaying points on same trip
@@ -58,7 +49,6 @@ def events():
 
     Pushtodb(snappedlist)
     return "ok"
-
 
 @app.route('/api', methods=['GET', 'POST'])
 def api():
@@ -72,9 +62,8 @@ def login():
     type_ = request.form.get('type')
     idn = NewDB.loginDB(type_)
 
-    print('login id:', idn, " and type", type_)
+    print('New login with id:', idn, " and type", type_)
     return str(idn)
-
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', threaded = True)
